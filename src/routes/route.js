1e6c173da3c664998d7f3458558e691acb1cfffe
sameerpanigrahi <panigrahisameer2000@gmail.com>
2022-08-05 19:06:02 +0530

    index.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,15 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/test-you', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+});
+
+router.get('/test-you', function (req, res) {
+    res.send('Hi  i am sameer')
+
 });
 
 
@@ -14,7 +19,7 @@ router.get('/test-you', function(req, res){
 })
 
 router.get('/give-me-students-data',function(req, res){
-
+ 
 })
 module.exports = router;
 // adding this comment for no reason
