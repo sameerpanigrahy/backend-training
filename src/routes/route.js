85207df9b7aeb3333637813bb1feac452ead1ec0
sameerpanigrahi <panigrahisameer2000@gmail.com>
2022-08-10 00:06:39 +0530

    this-IsCommitFor9ThAugAssignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,18 @@ const loggerModule = require('../logger/logger.js')
 const formatterModule = require('../validator/formatter') 
 const helperModule = require('../util/helper')
 const router = express.Router();
+const app=express()
+router.get('/candidates', function(req, res){
+  console.log('Query paramters for this request are '+JSON.stringify(req.query))
+  let gender = req.query.gender
+  let state = req.query.state
+  let district = req.query.district
+  console.log('State is '+state)
+  console.log('Gender is '+gender)
+  console.log('District is '+district)
+  let candidates = ['Akash','Suman']
+  res.send(candidates)
+})
 
 // router.get('/test-me', function (req, res) {
 //     console.log('My batch is', abc.name)
@@ -26,7 +38,7 @@ const router = express.Router();
 router.get('/test-me', function(req, res){
      const months=['jan','fab','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
      let result=lodash.chunk(months,3)
-     res.send(result);
+      //res.send(result);
      console.log(result);
      const array=[1,3,5,7,9,11,13,15,17,19]
        let tail=lodash.tail(array)
@@ -36,19 +48,43 @@ router.get('/test-me', function(req, res){
     const [a,b,c,d,e]=duplicate
     let sam=lodash.union(a,b,c,d,e)
     console.log(sam)
-    res.send(sam)
+    //res.send(sam)
      const oarray=[['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
     let object=lodash.fromPairs(oarray)
     console.log(object)
-     res.send(object)
+    // res.send(object)
 })
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
 })
 
-router.get('/give-me-students-data',function(req, res){
+app.get('/sol1',function(req, res){
+  const array=[1,2,3,5,6,7]
+  let sum=0
+  for (i=0;i<array.length;i++){
+    sum =sum+array[i]
+  }
+  const n=array.length+1
+  //console.log(n)
+  const missingNumber=((n*(n+1))/2)-sum//formula is (sumOfTheValueOfAnArray =(n*(n+1)/2)-missingvalue)
+  res.send({ data: missingNumber  }  )
+
+})
+app.get('/sol2',function(req, res){
+  const array=[33,34,35,37,38]
+  let sum=0
+  for (i=0;i<array.length;i++){
+    sum =sum +array[i]
+  }
+  const n=array.length+1
+  const fast=array.shift()
+  const last=array.pop()
+  const missingNumber=((n*(last+fast))/2)-sum
+  console.log(missingNumber)
+  res.send({ data: missingNumber  }  )
 
 })
 module.exports = router;
+module.exports=app;
 // adding this comment for no reason
