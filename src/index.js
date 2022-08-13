665ad95e3b4ae347f37a6627269f848b97ff8a93
sameerpanigrahi <panigrahisameer2000@gmail.com>
2022-08-13 15:32:22 +0530

    all commit are done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose} =require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://panigrahisameer_200:iklsSoxrtvpy4JOK@cluster0.kyd9m93.mongodb.net/Sameer6345-DB?retryWrites=true&w=majority",
+{
+    useNewUrlParser:true
+})
+.then(()=>console.log("mongodb is connected"))
+.catch(err=>console.log("err"))
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
