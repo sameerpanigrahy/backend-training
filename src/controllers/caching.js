584c1d3eafc7c15f2f4c104e26259dd2e2107632
vipulradon <vipulc777@gmail.com>
2022-10-06 11:26:26 +0530

    done

--- src/controllers/caching.js
+++ src/controllers/caching.js
@@ -33,19 +33,19 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 const fetchLongUrl = async function (req, res) {
 
 
-    const urlcode = req.params.urlCode;
-    if (!urlcode) return res.status(400).send({ status: false, message: "Please Enter A UrlCode" });
-    if (!shortId.isValid(urlcode)) return res.status(400).send({ status: false, message: "Please Check The UrlCode" });
-    
-  let cahcedProfileData = await GET_ASYNC(`${urlcode}`)
-  console.log(cahcedProfileData)
-  if(cahcedProfileData) {
-    return res.status(308).redirect(cahcedProfileData);
+  const urlcode = req.params.urlCode;
+  if (!urlcode) return res.status(400).send({ status: false, message: "Please Enter A UrlCode" });
+  if (!shortId.isValid(urlcode)) return res.status(400).send({ status: false, message: "Please Check The UrlCode" });
+
+  let cachedProfileData = await GET_ASYNC(`${urlcode}`)
+  console.log(cachedProfileData)
+  if (cachedProfileData) {
+    return res.status(308).redirect(cachedProfileData);
   } else {
     let data = await urlModel.findOne({ urlCode: urlcode }).select({ longUrl: 1 });
     if (!data) return res.status(404).send({ status: false, message: "No Url Found" });
-   let create= await SET_ASYNC(`${urlcode}`,data.longUrl )
-   console.log(create)
+    let create = await SET_ASYNC(`${urlcode}`, data.longUrl)
+    console.log(create)
     return res.status(308).redirect(data.longUrl);
   }
 
@@ -53,4 +53,4 @@ const fetchLongUrl = async function (req, res) {
 
 
 
-module.exports= {fetchLongUrl};
+module.exports = { fetchLongUrl };
